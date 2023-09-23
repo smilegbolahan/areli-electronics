ca8cc5d3dbc1539155ddd52cb0cce7ab49dd059e
smilegbolahan <42694929+smilegbolahan@users.noreply.github.com>
2023-09-23 17:19:00 +0100

    first commit

---
+++ core/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from core.models import CartOrderProducts, Product, Category, Vendor, CartOrder, ProductImages, ProductReview,  Address
+
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
+
+
+class CartOrderProductsAdmin(admin.ModelAdmin):
+    list_display = ['order', 'user', 'item','qty', 'price', 'total']
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesAdmin]
+    list_editable = ['title', 'price', 'featured', 'product_status']
+    list_display = ['user', 'title', 'product_image', 'price', 'category', 'brand', 'featured', 'product_status', 'pid']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category_image']
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['title', 'vendor_image']
+
+
+class CartOrderAdmin(admin.ModelAdmin):
+    list_editable = ['sku']
+    list_display = ['user',  'order_date', 'sku']
+
+
+
+
+
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'review', 'rating']
+
+
+class wishlistAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'date']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(CartOrder, CartOrderAdmin)
+
+
+admin.site.register(CartOrderProducts, CartOrderProductsAdmin)
+admin.site.register(ProductReview, ProductReviewAdmin)
+
